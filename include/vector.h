fadf5c47f45bd447834c77576e9876a088b0e0d1
MusaTamzid05 <tamzid101@gmail.com>
2021-02-14 03:51:57 +0600

    vector created.

---
+++ include/vector.h
@@ -0,0 +1,17 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+namespace Vector {
+
+    struct Vector2 {
+
+        Vector2(float x = 0.0f, float y = 0.0f);
+
+        float x;
+        float y;
+
+    };
+
+};
+
+#endif
---
+++ src/vector.cpp
@@ -0,0 +1,11 @@
+#include "vector.h"
+
+namespace Vector {
+
+    Vector2::Vector2(float x, float y):
+        x(x),
+        y(y) {}
+
+
+
+};
